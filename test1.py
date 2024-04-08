45a4f70bb7a6b9290e6a0cdb42d055216435722b
kristinalar <166371058+kristinalar@users.noreply.github.com>
2024-04-08 11:05:02 +0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a="hello" # строка
+a=[1,1.0, "hello"] # список
+a= (1, 1.0, "hello") # кортеж
+a={1,1.0, "hello"} # множество
+a=frozenset({1,1.0,"hello"}) # неизменяемое множество
+a={"a":1,"b":2} # словарь
