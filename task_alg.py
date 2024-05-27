7f45b41ce0198b79907417cdfbbb25758fc2d5ea
kristinalar <166371058+kristinalar@users.noreply.github.com>
2024-05-27 16:37:04 +0300

    Add files via upload

---
+++ task_alg.py
@@ -0,0 +1,32 @@
+# a, s, p = 1, 150, 200
+# while a <= 10:
+#     a += 2
+#     p += a
+#     s += p
+# print(f'Переменная s = {s}')
+#
+# a, s, p = 1, 150, 200
+# while True:
+#     if a > 10:
+#         break
+#     a += 2
+#     p += a
+#     s += p
+# print(f'Переменная s = {s}')
+
+# s = 1
+# for n in range(1, 6):
+#     s *= n
+# print('конец алгоритма')
+# print(f'Переменная s = {s}')
+
+# m, n = 12, 5
+# while True:
+#     if m == n:
+#         break
+#     elif m > n:
+#         m = m - 2*n
+#     else:
+#         n = n - 3
+# print(f'Переменная n = {n} и m = {m}')
+
