515ecb6f90323211531ff6022ea4554b9e5837b0
kristinalar <166371058+kristinalar@users.noreply.github.com>
2024-05-30 16:29:02 +0300

    Add files via upload

--- task_alg.py
+++ task_alg.py
@@ -30,3 +30,36 @@
 #         n = n - 3
 # print(f'Переменная n = {n} и m = {m}')
 
+k, l = [ ], [ ]
+for i in range(1, 11):
+    k.append(10 -i)
+for i in range(1, 11):
+    l.append(k[5] - k[i - 1])
+print(k)
+print(l)
+
+k, l = list(range(1, 11)), list(range(1, 11))
+for i in range(1, 11):
+    k[i - 1] = 10 - i
+for i in range(1, 11):
+    l[i - 1] = k[5] - k[i - 1]
+print(k)
+print(l)
+print(f'Количество отрицательных элементов массива = {len([value for value in l if value < 0])}')
+
+k, l = list(range(1, 11)), list(range(1, 11))
+for i in range(1, 11):
+    k[i - 1] = 10 - i
+for i in range(1, 11):
+    l[i - 1] = k[5] - k[i - 1]
+print(k)
+print(l)
+count_k = 0
+for value in k:
+    if value < 0:
+        count_k += 1
+count_l = 0
+for value in l:
+    if value < 0:
+        count_l += 1
+print(f'Количество отрицательных элементов = {count_k + count_l}')
